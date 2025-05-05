65f7eb7f40d1ca549fd490dcb41bb9a57451d6a4
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2025-05-05 16:25:59 -0300

    react&node

--- server.js
+++ server.js
@@ -6,17 +6,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let ultimaMensagem = 'Nenhuma mensagem recebida ainda';
+
 app.post('/api/dados', (req, res) => {
     console.log('Dados recebidos do Unity:', req.body);
     res.json({ status: 'Recebido com sucesso' });
 });
 
-//quero cadastrar um novo usuario
 app.post('/api/cadastrar', (req, res) => {
-    const msgReact = req.body.msg;
-    console.log('Recebido do React:', msgReact);
-    res.json({ resposta: 'Oi Unity, recebi isso: ' + msgReact });
+    ultimaMensagem = req.body.msg; // salva a msg
+    console.log('Recebido do React:', ultimaMensagem);
+    res.json({ status: 'Mensagem salva' });
 });
+
+app.get('/api/cadastrar', (req, res) => {
+    res.json({ mensagem: "Olá unity está é a ultima msg do react", ultimaMensagem }); // Unity busca isso
+});
+
 
 app.listen(3001, () => {
     console.log('Servidor rodando na porta 3001');
