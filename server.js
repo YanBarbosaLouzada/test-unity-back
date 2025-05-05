0bc3fe81250fc134d6830b1d93069c28cb34ca56
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2025-05-05 11:20:55 -0300

    node&unity

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/dados', (req, res) => {
+    console.log('Dados recebidos do Unity:', req.body);
+    res.json({ status: 'Recebido com sucesso' });
+});
+
+app.listen(3001, () => {
+    console.log('Servidor rodando na porta 3001');
+});
